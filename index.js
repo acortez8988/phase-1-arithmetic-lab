4bd6fbe1004db8a7aa04a24484126d5976a788a8
Alexander Cortez <acortez8988@gmail.com>
2022-07-05 11:19:04 -0400

    commit tree ed0b4adc19583c35ac81b37c84e36917042d6c9f

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
-// Write your code here
+    
+//    function multiply(num1,num2) {
+//     let num1= 1;
+//     let num2= 31;
+//       return num1 * num2;
+//    }
+const num1= 2
+const num2= 31
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random()*10)+1;
+
